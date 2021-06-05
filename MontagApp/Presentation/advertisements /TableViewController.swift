16aa710629b59843e9e4df9a9759a0cf1d0322bd
Anas Mohamed <anaskamal659@gmail.com>
2021-06-04 18:24:56 -0700

    move to login from menu

--- MontagApp/Presentation/advertisements /TableViewController.swift
+++ MontagApp/Presentation/advertisements /TableViewController.swift
@@ -29,22 +29,34 @@ class SideMenuTableViewController: UITableViewController {
             return
         }
         tableView.register(UITableViewVibrantCell.self, forCellReuseIdentifier: "UITableViewVibrantCell")
-       
+        
         // Set up a cool background image for demo purposes
-//        let imageView = UIImageView(image: #imageLiteral(resourceName: "saturn"))
-//        imageView.contentMode = .scaleAspectFit
-//        imageView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-//        tableView.backgroundView = imageView
+        //        let imageView = UIImageView(image: #imageLiteral(resourceName: "saturn"))
+        //        imageView.contentMode = .scaleAspectFit
+        //        imageView.backgroundColor = UIColor.black.withAlphaComponent(0.2)
+        //        tableView.backgroundView = imageView
     }
     
+    @IBAction func signInBtnDidTapped(_ sender: Any) {
+        let loginViewStorboard = UIStoryboard(name: "LoginView", bundle: nil)
+        let loginViewController = loginViewStorboard.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        
+        //        loginViewController.modalPresentationStyle = .fullScreen
+        let navigationController = UINavigationController.init(rootViewController: loginViewController)
+        
+        navigationController.modalPresentationStyle = .fullScreen
+        
+        self.present(navigationController, animated: true, completion: nil)
+        //        self.present(loginViewController, animated: true, completion: nil)
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAt: indexPath) as! UITableViewVibrantCell
-       
+        
         if let menu = navigationController as? SideMenuNavigationController {
             cell.blurEffectStyle = menu.blurEffectStyle
         }
         
         return cell
     }
-
+    
 }
